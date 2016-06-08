b0edf379dc466b37ba2931061f6f2364ad9f939b
Wen Jia Liu <wj32.64@gmail.com>
2016-06-08 18:40:20 +1000

    Add Result monad functions

--- Wj.Async/Result.fs
+++ Wj.Async/Result.fs
@@ -3,13 +3,47 @@
 open System
 
 module Result =
-  type T<'a, 'b> =
+  type T<'a, 'error> =
     | Success of 'a
-    | Failure of 'b
+    | Failure of 'error
 
-  let ofSuccess x = Success x
+  let ``return`` x = Success x
 
-  let ofFailure y = Failure y
+  let bind t f = match t with Success x -> f x | Failure error -> Failure error
+
+  let map t f = match t with Success x -> Success (f x) | Failure error -> Failure error
+
+  let join t = bind t id
+
+  let forget t = map t ignore
+
+  let all ts =
+    let rec loop acc ts =
+      match ts with
+      | [] -> Success (List.rev acc)
+      | t :: ts ->
+        match t with
+        | Success x -> loop (x :: acc) ts
+        | Failure error -> Failure error
+    loop [] ts
+
+  let allForget ts =
+    let rec loop ts =
+      match ts with
+      | [] -> Success ()
+      | t :: ts ->
+        match t with
+        | Success () -> loop ts
+        | Failure error -> Failure error
+    loop ts
+
+  module Infix =
+    let (>>=) t f = bind t f
+    let (>>|) t f = map t f
+
+  let ofSuccess x = ``return`` x
+
+  let ofFailure error = Failure error
 
   let tryWith f =
     try
--- Wj.Async/Result.fsi
+++ Wj.Async/Result.fsi
@@ -3,11 +3,31 @@
 open System
 
 module Result =
-  type T<'a, 'b> =
+  type T<'a, 'error> =
     | Success of 'a
-    | Failure of 'b
+    | Failure of 'error
 
-  val ofSuccess : 'a -> T<'a, 'b>
-  val ofFailure : 'b -> T<'a, 'b>
+  // Monad
+
+  val ``return`` : 'a -> T<'a, 'error>
+  val bind : T<'a, 'error> -> ('a -> T<'b, 'error>) -> T<'b, 'error>
+  val map : T<'a, 'error> -> ('a -> 'b) -> T<'b, 'error>
+  val join : T<T<'a, 'error>, 'error> -> T<'a, 'error>
+  val forget : T<'a, 'error> -> T<unit, 'error>
+  val all : T<'a, 'error> list -> T<'a list, 'error>
+  val allForget : T<unit, 'error> list -> T<unit, 'error>
+
+  // Infix operators
+
+  module Infix =
+    val (>>=) : T<'a, 'error> -> ('a -> T<'b, 'error>) -> T<'b, 'error>
+    val (>>|) : T<'a, 'error> -> ('a -> 'b) -> T<'b, 'error>
+
+  // Conversion
+
+  val ofSuccess : 'a -> T<'a, 'error> // same as ``return``
+  val ofFailure : 'error -> T<'a, 'error>
+
+  // Try with
 
   val tryWith : (unit -> 'a) -> T<'a, exn>
