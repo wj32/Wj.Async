6184043f5f36943755fd510a8ab9a08ab7c030e1
Wen Jia Liu <wj32.64@gmail.com>
2016-06-10 16:55:01 +1000

    Reorganize DeferredBuilder

--- Wj.Async/DeferredBuilder.fs
+++ Wj.Async/DeferredBuilder.fs
@@ -3,57 +3,61 @@
 open System
 open System.Collections.Generic
 
-type DeferredBuilder() =
-  member inline this.Bind(t, f) = Deferred.bind t f
+module DeferredBuilder =
+  type 'a T = 'a IDeferred
+  type 'a M = 'a IDeferred
 
-  member inline this.Combine(t, f) = Deferred.bind t f
+  type B() =
+    member inline this.Bind(t, f) = Deferred.bind t f
 
-  member inline this.Delay(f) = f
+    member inline this.Combine(t : unit M, f) = Deferred.bind t f
 
-  member inline this.Return(x) = Deferred.value x
+    member inline this.Delay(f) = f
 
-  member inline this.ReturnFrom(t) = t
+    member inline this.Return(x) = Deferred.value x
 
-  member inline this.Run(f) = f ()
+    member inline this.ReturnFrom(t) = t
 
-  member inline this.Zero() = Deferred.unit
+    member inline this.Run(f) = f ()
 
-  member inline this.TryFinally(body, finalizer) =
-    Supervisor.tryFinally body finalizer
+    member inline this.Zero() = Deferred.unit
 
-  member inline this.TryFinally(body, finalizer) =
-    Supervisor.tryFinally body (fun () -> finalizer (); Deferred.unit)
+    member inline this.TryFinally(body, finalizer) =
+      Supervisor.tryFinally body finalizer
 
-  member inline this.TryWith(body, handler) =
-    Supervisor.tryWith body handler Supervisor.AfterDetermined.Log
+    member inline this.TryFinally(body, finalizer) =
+      Supervisor.tryFinally body (fun () -> finalizer (); Deferred.unit)
 
-  member inline this.Using(disposable : #IDisposable, body) =
-    // TODO: If disposable is a struct, it will get boxed in the check below. Find a way of handling
-    // this.
-    Supervisor.tryFinally
-      (fun () -> body disposable)
-      (fun () -> (if not (obj.ReferenceEquals(disposable, null)) then disposable.Dispose()); Deferred.unit)
+    member inline this.TryWith(body, handler) =
+      Supervisor.tryWith body handler Supervisor.AfterDetermined.Log
 
-  member inline this.WhileGeneric(upon, bind, guard, body : unit -> unit IDeferred) =
-    bind (guard ()) (fun b ->
-      if b then
-        let v = Deferred.createVar ()
-        let rec loop () =
-          Deferred.upon (body ()) (fun () ->
-            upon (guard ()) (fun b -> if b then loop () else Deferred.set v ())
-          )
-        loop ()
-        v :> _ IDeferred
-      else
-        Deferred.unit
-    )
+    member inline this.Using(disposable : #IDisposable, body) =
+      // TODO: If disposable is a struct, it will get boxed in the check below. Find a way of handling
+      // this.
+      Supervisor.tryFinally
+        (fun () -> body disposable)
+        (fun () -> (if not (obj.ReferenceEquals(disposable, null)) then disposable.Dispose()); Deferred.unit)
 
-  member this.While(guard, body : unit -> unit IDeferred) =
-    this.WhileGeneric((|>), (|>), guard, body)
+    member inline this.WhileGeneric(upon, bind, guard, body : unit -> unit M) =
+      bind (guard ()) (fun b ->
+        if b then
+          let v = Deferred.createVar ()
+          let rec loop () =
+            Deferred.upon (body ()) (fun () ->
+              upon (guard ()) (fun b -> if b then loop () else Deferred.set v ())
+            )
+          loop ()
+          v :> _ IDeferred
+        else
+          Deferred.unit
+      )
 
-  member this.While(guard : unit -> bool IDeferred, body : unit -> unit IDeferred) =
-    this.WhileGeneric(Deferred.upon, Deferred.bind, guard, body)
+    member this.While(guard, body : unit -> unit M) =
+      this.WhileGeneric((|>), (|>), guard, body)
 
-  member inline this.For(xs, body) = Deferred.Seq.iter Parallelism.Sequential body xs
+    member this.While(guard : unit -> bool M, body : unit -> unit M) =
+      this.WhileGeneric(Deferred.upon, Deferred.bind, guard, body)
 
-  member inline this.For(xs, body) = DeferredSeq.iter' body xs
+    member inline this.For(xs, body) = Deferred.Seq.iter Parallelism.Sequential body xs
+
+    member inline this.For(xs, body) = DeferredSeq.iter' body xs
--- Wj.Async/DeferredBuilder.fsi
+++ Wj.Async/DeferredBuilder.fsi
@@ -2,20 +2,24 @@
 
 open System
 
-type DeferredBuilder =
-  new : unit -> DeferredBuilder
-  member inline Bind : 'a IDeferred * ('a -> 'b IDeferred) -> 'b IDeferred
-  member inline Combine : unit IDeferred * (unit -> 'b IDeferred) -> 'b IDeferred
-  member inline Delay : 'f -> 'f
-  member inline Return : 'a -> 'a IDeferred
-  member inline ReturnFrom : 'd -> 'd
-  member inline Run : (unit -> 'd) -> 'd
-  member inline Zero : unit -> unit IDeferred
-  member inline TryFinally : body : (unit -> 'a IDeferred) * finalizer : (unit -> unit IDeferred) -> 'a IDeferred
-  member inline TryFinally : body : (unit -> 'a IDeferred) * finalizer : (unit -> unit) -> 'a IDeferred
-  member inline TryWith : body : (unit -> 'a IDeferred) * handler : (exn -> 'a IDeferred) -> 'a IDeferred
-  member inline Using : disposable : 'disposable * body : ('disposable -> 'a IDeferred) -> 'a IDeferred when 'disposable :> IDisposable
-  member While : guard : (unit -> bool) * body : (unit -> unit IDeferred) -> unit IDeferred
-  member While : guard : (unit -> bool IDeferred) * body : (unit -> unit IDeferred) -> unit IDeferred
-  member inline For : xs : 'a seq * body : ('a -> unit IDeferred) -> unit IDeferred
-  member inline For : xs : 'a DeferredSeq.T * body : ('a -> unit IDeferred) -> unit IDeferred
+module DeferredBuilder =
+  type 'a T = 'a IDeferred
+  type 'a M = 'a T
+
+  type B =
+    new : unit -> B
+    member inline Bind : 'a M * ('a -> 'b M) -> 'b M
+    member inline Combine : unit M * (unit -> 'b M) -> 'b M
+    member inline Delay : 'f -> 'f
+    member inline Return : 'a -> 'a M
+    member inline ReturnFrom : 'd -> 'd
+    member inline Run : (unit -> 'd) -> 'd
+    member inline Zero : unit -> unit M
+    member inline TryFinally : body : (unit -> 'a M) * finalizer : (unit -> unit M) -> 'a M
+    member inline TryFinally : body : (unit -> 'a M) * finalizer : (unit -> unit) -> 'a M
+    member inline TryWith : body : (unit -> 'a M) * handler : (exn -> 'a M) -> 'a M
+    member inline Using : disposable : 'disposable * body : ('disposable -> 'a M) -> 'a M when 'disposable :> IDisposable
+    member While : guard : (unit -> bool) * body : (unit -> unit M) -> unit M
+    member While : guard : (unit -> bool M) * body : (unit -> unit M) -> unit M
+    member inline For : xs : 'a seq * body : ('a -> unit M) -> unit M
+    member inline For : xs : 'a DeferredSeq.T * body : ('a -> unit M) -> unit M
