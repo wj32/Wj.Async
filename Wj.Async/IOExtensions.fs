5a4ccc3d9c106ce33828c69dd9c0f37b0b5cf39c
Wen Jia Liu <wj32.64@gmail.com>
2016-06-26 19:41:15 -0700

    Add TextReader and TextWriter extensions

--- Wj.Async/IOExtensions.fs
+++ Wj.Async/IOExtensions.fs
@@ -7,29 +7,14 @@ module IOExtensions =
   open Deferred.Infix
 
   type Stream with
-    member t.FlushDeferred() =
-      let supervisor = Supervisor.current ()
-      Deferred.create (fun v ->
-        System.Threading.ThreadPool.QueueUserWorkItem(fun _ ->
-          try
-            t.Flush() --> v
-          with ex ->
-            supervisor.SendException(ex)
-        ) |> ignore
-      )
+    member t.FlushDeferred() = Deferred.ofTaskUnit (t.FlushAsync())
 
     member t.ReadDeferred(buffer, ?offset, ?count) =
-      let supervisor = Supervisor.current ()
       let offset = defaultArg offset 0
       let count = defaultArg count (Array.length buffer)
-      Deferred.create (fun v ->
-        t.BeginRead(buffer, offset, count, (fun result ->
-          try
-            t.EndRead(result) --> v
-          with ex ->
-            supervisor.SendException(ex)
-        ), null) |> ignore
-      )
+      Deferred.ofBeginEnd
+        (fun callback -> t.BeginRead(buffer, offset, count, callback, null) |> ignore)
+        t.EndRead
 
     member t.ReadDeferred(count) =
       let buffer = Array.zeroCreate count
@@ -41,14 +26,44 @@ module IOExtensions =
         Array.sub buffer 0 bytesRead
 
     member t.WriteDeferred(buffer, ?offset, ?count) =
-      let supervisor = Supervisor.current ()
       let offset = defaultArg offset 0
       let count = defaultArg count (Array.length buffer)
-      Deferred.create (fun v ->
-        t.BeginWrite(buffer, offset, count, (fun result ->
-          try
-            t.EndWrite(result) --> v
-          with ex ->
-            supervisor.SendException(ex)
-        ), null) |> ignore
-      )
+      Deferred.ofBeginEnd
+        (fun callback -> t.BeginWrite(buffer, offset, count, callback, null) |> ignore)
+        t.EndWrite
+
+  type TextReader with
+    member t.ReadDeferred(buffer, ?index, ?count) =
+      let index = defaultArg index 0
+      let count = defaultArg count (Array.length buffer)
+      Deferred.ofTask (t.ReadAsync(buffer, index, count))
+
+    member t.ReadBlockDeferred(buffer, ?index, ?count) =
+      let index = defaultArg index 0
+      let count = defaultArg count (Array.length buffer)
+      Deferred.ofTask (t.ReadBlockAsync(buffer, index, count))
+
+    member t.ReadLineDeferred() = Deferred.ofTask (t.ReadLineAsync())
+
+    member t.ReadToEndDeferred() = Deferred.ofTask (t.ReadToEndAsync())
+
+  type TextWriter with
+    member t.FlushDeferred() = Deferred.ofTaskUnit (t.FlushAsync())
+
+    member t.WriteDeferred(value : char) = Deferred.ofTaskUnit (t.WriteAsync(value))
+
+    member t.WriteDeferred(value : string) = Deferred.ofTaskUnit (t.WriteAsync(value))
+
+    member t.WriteDeferred(buffer, ?index, ?count) =
+      let index = defaultArg index 0
+      let count = defaultArg count (Array.length buffer)
+      Deferred.ofTaskUnit (t.WriteAsync(buffer, index, count))
+
+    member t.WriteLineDeferred(value : char) = Deferred.ofTaskUnit (t.WriteLineAsync(value))
+
+    member t.WriteLineDeferred(value : string) = Deferred.ofTaskUnit (t.WriteLineAsync(value))
+
+    member t.WriteLineDeferred(buffer, ?index, ?count) =
+      let index = defaultArg index 0
+      let count = defaultArg count (Array.length buffer)
+      Deferred.ofTaskUnit (t.WriteLineAsync(buffer, index, count))
--- Wj.Async/IOExtensions.fsi
+++ Wj.Async/IOExtensions.fsi
@@ -9,3 +9,18 @@ module IOExtensions =
     member ReadDeferred : buffer : byte array * ?offset : int * ?count : int -> int IDeferred
     member ReadDeferred : count : int -> byte array IDeferred
     member WriteDeferred : buffer : byte array * ?offset : int * ?count : int -> unit IDeferred
+
+  type TextReader with
+    member ReadDeferred : buffer : char array * ?index : int * ?count : int -> int IDeferred
+    member ReadBlockDeferred : buffer : char array * ?index : int * ?count : int -> int IDeferred
+    member ReadLineDeferred : unit -> string IDeferred
+    member ReadToEndDeferred : unit -> string IDeferred
+
+  type TextWriter with
+    member FlushDeferred : unit -> unit IDeferred
+    member WriteDeferred : value : char -> unit IDeferred
+    member WriteDeferred : value : string -> unit IDeferred
+    member WriteDeferred : buffer : char array * ?index : int * ?count : int -> unit IDeferred
+    member WriteLineDeferred : value : char -> unit IDeferred
+    member WriteLineDeferred : value : string -> unit IDeferred
+    member WriteLineDeferred : buffer : char array * ?index : int * ?count : int -> unit IDeferred
