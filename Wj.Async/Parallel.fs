6e893b3d69966c33867c8f8a33df827f07308a41
Wen Jia Liu <wj32.64@gmail.com>
2017-03-25 14:56:15 -0700

    Add Parallel stubs

---
+++ Wj.Async/Parallel.fs
@@ -0,0 +1,42 @@
+﻿namespace Wj.Async
+
+open System
+open Wj.Async.Internal
+
+module Parallel =
+  [<Interface>]
+  type 'a IDequeueable =
+    abstract member Lock : obj
+    abstract member IsEmpty : bool
+    abstract member Dequeue : unit -> 'a
+
+  module DequeuingDispatcher =
+    [<ReferenceEquality>]
+    type T =
+      { mutable rootSupervisor : ISupervisor;
+        sharedQueue : unit SupervisedCallback IDequeueable;
+        privateQueue : unit SupervisedCallback Queue.T; }
+
+  module Pool =
+    type T =
+      | Dummy
+
+    module Config =
+      type T =
+        { minThreadCount : int;
+          maxThreadCount : int;
+          threadTimeout : TimeSpan option; }
+
+      let ``default`` =
+        { minThreadCount = 0;
+          maxThreadCount = Environment.ProcessorCount;
+          threadTimeout = Some (TimeSpan.FromSeconds(10.)); }
+
+    let create (_ : Config.T) =
+      Dummy
+
+    let ``default`` = create Config.``default``
+
+  let parallelDefault = Concurrency.concurrent
+
+  let parallelAtMost (_ : int) = (failwith "Not implemented") : IConcurrency
---
+++ Wj.Async/Parallel.fsi
@@ -0,0 +1,22 @@
+﻿namespace Wj.Async
+
+open System
+
+module Parallel =
+  module Pool =
+    [<Sealed>]
+    type T
+
+    module Config =
+      type T =
+        { minThreadCount : int;
+          maxThreadCount : int;
+          threadTimeout : TimeSpan option; }
+
+      val ``default`` : T
+
+    val create : Config.T -> T
+    val ``default`` : T
+
+  val parallelDefault : IConcurrency
+  val parallelAtMost : maxThreadCount : int -> IConcurrency
